54d76da207acd905234fe1e68bf038492ef9e818
Eric Su <esericsu@gmail.com>
2020-05-30 15:12:19 -0700

    add the implementation of background

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -21,23 +21,30 @@ const randomCommand = () => {
 
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
+  // http://127.0.0.1:3000
   //127.0.0.0.1:3000  <---commands
   if (req.method === 'GET') {
     console.log('received GET request')
-//respond with 200
-
-//respond with 'dequeue'
-
-    if (messageQueue) {
-      res.writeHead(200, headers);
-      var command = messageQueue.dequeue()
-      console.log('command from router', command);
-      res.end(command);
+//if url is '/'
+    if (req.url === '/') {
+      if (messageQueue) {
+        res.writeHead(200, headers);
+        var command = messageQueue.dequeue()
+        console.log('command from router', command);
+        res.end(command);
+      }
     }
+    // url is '/background.jpg'
+    if(req.url === '/background.jpg'){
+      res.writeHead(200, headers);
+
+    }
+
+
   }
+
   next(); // invoke next() at the end of a request to help with testing!
 //127.0.0.1:3000/background.jpg
-if(req.method === 'GET' && req.url === 'http://127.0.0.1:3000/background.jpg') {
 
-}
+
 };
